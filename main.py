29c91568b6fadea6d56c6390b9ed6af2fdfab0e5
david <davidliat98@gmail.com>
2023-08-07 18:35:40 -0400

    add find string functions

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_news(url):
+
+def scrape_routes(url):
     response = requests.get(url)
-    print(response)
+    print(response, news_url)
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        headlines = []
-        
-        for article in soup.find_all('div'):
-            headline = article.find('h3')
-            if headline and headline.find('a'):
-                linkline = headline.find('a')
-                link = linkline['href'] if linkline else ''
-                headlines.append({'title': headline.get_text(strip = True), 'link': link})
-                
-        return headlines
+        soup = BeautifulSoup(response.text, "html.parser")
+        a_tags = soup.find_all("a")
+        for tag in a_tags:
+            link = tag.get("href", "")
+            if link and link not in routes:
+                if link.startswith("/en"):
+                    routes.append("https://www.pinkelephant.com" + link)
     else:
-        print('Failed to retrieve the page.')
-        
-if __name__ == '__main__':
-    news_url = 'https://www.pinkelephant.com/en-us/'
-    news_headlines = scrape_news(news_url)
-    
-    if news_headlines:
-        print('Latest News Headlines:')
-        for index, news in enumerate(news_headlines, start = 1):
-            print(f"{index}. {news['title']}")
-            print(f"    Link: {news['link']}")
-            print()
-    else:
-        print('No news headlines found.')
-            
+        print("Failed to retrieve the page.")
+
+
+def scrape_news():
+    headlines = []
+    for url in routes:
+        response = requests.get(url)
+        print(response, url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            # print(routes)
+
+            for element in soup.find_all("div"):
+                headlines.append(
+                    {"url": url, "article": element.get_text(strip=True).casefold()}
+                )
+
+        else:
+            print("Failed to retrieve the page.")
+    return headlines
+
+
+def find_string(articles, target_string):
+    found_strings = [
+        {"url": data["url"], "string": target_string, "element": data["article"]}
+        for data in articles
+        if target_string in data["article"]
+    ]
+    return found_strings
+
+
+if __name__ == "__main__":
+    routes = []
+    news_url = "https://www.pinkelephant.com/en-us"
+    target_string = "example string"
+    headlines = scrape_routes(news_url)
+    # print(routes)
+    news_headlines = scrape_news()
+    found_strings = find_string(news_headlines, target_string)
+
+    print(found_strings)
