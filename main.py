a7879bc21d955e47d081d092d707357688858e94
David Y <d.yang@pinkelephant.com>
2023-08-14 15:05:06 -0400

    add error handling

--- main.py
+++ main.py
@@ -5,27 +5,36 @@ from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
 
 # tutorials: https://www.youtube.com/watch?v=fKl2JW_qrso
 
-
-def scrape_routes(soup, region, routes):
-    for tag in soup.find_all("a"):
-        link = tag.get("href", "").casefold()
-        full_link = news_url + link
-        if (
-            link
-            and link.startswith(f"/{region}/course")
-            and (link not in routes[region] and full_link not in routes[region])
-        ):
-            routes[region].add(full_link)
-
-
-def scrape_course_info(region_code, route_url):
+def request_handle(route_url):
     response = requests.get(route_url)
     print(response, route_url)
-
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
-        scrape_routes(soup, region_code, routes)
+        return soup
+    else:
+        print("Failed to retrieve the page.")
+        return None
+    
+def scrape_routes(region, route):
+    soup = request_handle(route)
+    if soup:
+        for tag in soup.find_all("a"):
+            link = tag.get("href", "").casefold()
+            full_link = news_url + link
+            if (
+                link
+                and link.startswith(f"/{region}/course")
+                and (link not in routes[region] and full_link not in routes[region])
+            ):
+                routes[region].add(full_link)
+    else: raise ConnectionError('Failed to connect to the page')
 
+
+def scrape_course_info(region_code, route):
+    
+    soup = request_handle(route)
+
+    if soup:
         course_name = soup.title.string.replace(",", " ")
 
         # process only in course detail page
@@ -46,7 +55,7 @@ def scrape_course_info(region_code, route_url):
                     {
                         "region code": region_code,
                         "id": course_id,
-                        "url": route_url,
+                        "url": route,
                         "course": course_name,
                         "currency": course_currency,
                         "date": course_date,
@@ -76,27 +85,28 @@ if __name__ == "__main__":
     start = time.perf_counter()
     news_url = "https://www.pinkelephant.com"
     region_codes = ["en-us", "en-ca"]
+    global routes
     routes = {region_code: set() for region_code in region_codes}
     course_infos = []
+    try:
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            for region_code in region_codes:
+                base_url = f"{news_url}/{region_code}/products/training"
+                routes[region_code].add(base_url)
+                
+                futures = [executor.submit(scrape_routes, region_code, route) for route in routes[region_code]]
 
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = []
-        for region_code in region_codes:
-            base_url = f"{news_url}/{region_code}/products/training"
-            routes[region_code].add(base_url)
-            for route in routes[region_code]:
-                futures.append(executor.submit(scrape_course_info, region_code, route))
-
-            wait(futures, return_when=ALL_COMPLETED)
-            # append to course_infos
-            executor.map(
-                scrape_course_info,
-                [region_code] * len(routes[region_code]),
-                routes[region_code],
-            )
-
-    # print(course_infos)
-    export_csv(course_infos)
+                wait(futures, return_when=ALL_COMPLETED)
+                # append to course_infos
+                executor.map(
+                    scrape_course_info,
+                    [region_code] * len(routes[region_code]),
+                    routes[region_code],
+                )
+        # print(course_infos)
+        export_csv(course_infos)
+    except Exception as error:
+        print(f'{error}')
 
     finish = time.perf_counter()
-    print(f"Finish in {round(start - finish, 2)} secs")
+    print(f"Finish in {round(finish - start, 2)} secs")
